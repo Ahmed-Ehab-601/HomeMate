7588ced7763789df00ade89f5e531c82eabd63b6
Ahmed-Ehab-601 <medo.losy@gmail.com>
2025-11-25 13:23:19 +0200

    solve all merge conflict with milesone one tasker and task stories all mvn clean test pass

--- homemate-backend/src/test/java/com/homemate/taskmanagementtests/TaskDaoImpIntegrationTest.java
+++ homemate-backend/src/test/java/com/homemate/taskmanagementtests/TaskDaoImpIntegrationTest.java
@@ -26,7 +26,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("task")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class TaskDaoImpIntegrationTest {
 
     private final TaskDaoImpl underTest;
