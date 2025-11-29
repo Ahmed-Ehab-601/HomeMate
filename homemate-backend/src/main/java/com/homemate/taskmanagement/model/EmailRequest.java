b74e76bcf6a9b9e79e9f68195cc4e78a840bf753
Ahmed-Ehab-601 <medo.losy@gmail.com>
2025-11-29 19:28:50 +0200

    commented 2 model classes
    comment till their test is ready in milestone 2

--- homemate-backend/src/main/java/com/homemate/taskmanagement/model/EmailRequest.java
+++ homemate-backend/src/main/java/com/homemate/taskmanagement/model/EmailRequest.java
@@ -3,9 +3,9 @@ package com.homemate.taskmanagement.model;
 import com.homemate.taskmanagement.dto.TaskDto;
 import lombok.Data;
 
-@Data
-public class EmailRequest {
-    private TaskDto task;
-    private EmailType type;
-    private String recipientEmail;
-}
+//@Data
+//public class EmailRequest {
+//    private TaskDto task;
+//    private EmailType type;
+//    private String recipientEmail;
+//}
