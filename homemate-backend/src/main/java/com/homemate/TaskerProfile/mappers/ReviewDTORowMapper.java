55685959e2bdf93427514ab3c02fb01080db61bf
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2025-12-07 21:34:42 +0200

    adding View reviews in the tasker hub with images + adding test images in the database

--- homemate-backend/src/main/java/com/homemate/TaskerProfile/mappers/ReviewDTORowMapper.java
+++ homemate-backend/src/main/java/com/homemate/TaskerProfile/mappers/ReviewDTORowMapper.java
@@ -20,6 +20,8 @@ public class ReviewDTORowMapper implements RowMapper<ReviewDTO> {
         review.setRate(rs.getDouble("rate"));
         review.setTime(rs.getTimestamp("time"));
         review.setTaskId(rs.getInt("taskID"));
+        review.setReviewerUsername(rs.getString("reviewerUsername"));
+       
         return review;
     }
 }
