b12787f74ea282ac01efb5e70688b96528015496
Ahmed-Ehab-601 <medo.losy@gmail.com>
2025-12-09 00:05:37 +0200

    resolve conflict with milestone_2

--- homemate-backend/src/main/java/com/homemate/taskmanagement/controller/TaskManagementController.java
+++ homemate-backend/src/main/java/com/homemate/taskmanagement/controller/TaskManagementController.java
@@ -3,7 +3,6 @@ package com.homemate.taskmanagement.controller;
 
 import com.homemate.security.model.AppUserDetails;
 import com.homemate.taskmanagement.dto.*;
-import com.homemate.taskmanagement.exceptions.BadTaskRequestException;
 import com.homemate.taskmanagement.model.Status;
 import com.homemate.taskmanagement.service.TaskManagementService;
 import jakarta.validation.Valid;
@@ -15,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
