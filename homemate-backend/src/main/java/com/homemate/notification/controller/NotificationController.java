631126a7562648b03d3977caf165cf4afec720bf
SamaaIbrahim <173613399+SamaaIbrahim@users.noreply.github.com>
2025-12-10 00:15:10 +0200

    implement emailservice

--- homemate-backend/src/main/java/com/homemate/notification/controller/NotificationController.java
+++ homemate-backend/src/main/java/com/homemate/notification/controller/NotificationController.java
@@ -1,12 +1,36 @@
 package com.homemate.notification.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.homemate.notification.domains.dto.EmailRequest;
+import com.homemate.notification.service.EmailService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/notifications")
+@RequiredArgsConstructor
 public class NotificationController {
-//
+	private final EmailService emailService;
 
+	@PostMapping("/user")
+	public ResponseEntity<Map<String, String>> sendUserEmail(
+			@Valid @RequestBody EmailRequest emailRequest
+	) {
+		emailService.sendUserEmail(emailRequest);
+		return ResponseEntity.accepted().body(Map.of("message", "Notification sent to user"));
+	}
+
+	@PostMapping("/tasker")
+	public ResponseEntity<Map<String, String>> sendTaskerEmail(
+			@Valid @RequestBody EmailRequest emailRequest
+	) {
+		emailService.sendTaskerEmail(emailRequest);
+		return ResponseEntity.accepted().body(Map.of("message", "Notification sent to tasker"));
+	}
 }
--- homemate-backend/src/main/java/com/homemate/notification/service/imp/TaskServiceImp.java
+++ homemate-backend/src/main/java/com/homemate/notification/service/imp/TaskServiceImp.java
@@ -1,4 +1,58 @@
 package com.homemate.notification.service.imp;
 
-public class TaskServiceImp {
+import com.homemate.notification.domains.dto.EmailRequest;
+import com.homemate.notification.service.EmailService;
+import com.homemate.notification.service.utils.EmailTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+public class EmailServiceImp implements EmailService {
+    private EmailTemplate emailTemplate;
+    private JavaMailSender javaMailSender;
+    private static final String HOMEMATE_EMAIL ="homematesevice8@gmail.com";
+    public EmailServiceImp(EmailTemplate emailTemplate,JavaMailSender javaMailSender) {
+        this.emailTemplate = emailTemplate;
+        this.javaMailSender=javaMailSender;
+    }
+    private void sendNotification(String subject , String body , EmailRequest emailRequest) {
+        SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+        simpleMailMessage.setTo(emailRequest.getRecipientEmail());
+        simpleMailMessage.setFrom(HOMEMATE_EMAIL);
+        javaMailSender.send(simpleMailMessage);
+    }
+    @Override
+    public void sendUserEmail(EmailRequest emailRequest) {
+        EmailRequest.EmailType emailType=emailRequest.getEmailType();
+        switch (emailType){
+            case TASK_ACCEPTED,TASK_STATUS,TASK_REJECTED -> sendNotification(emailTemplate.buildEmailSubject(emailRequest),emailTemplate.buildTaskStatusChangedBody(emailRequest.getTask()),emailRequest);
+            case TASK_RESCHEDULE -> sendNotification(emailTemplate.buildEmailSubject(emailRequest),emailTemplate.buildUserTaskRescheduleBody(emailRequest.getTask()),emailRequest);
+            case TASK_RESUMED -> sendNotification(emailTemplate.buildEmailSubject(emailRequest),emailTemplate.BuildTaskResumedBody(emailRequest.getTask()),emailRequest);
+            default -> {
+                log.error("Unsupported email type: {}", emailType);
+                return;
+            }
+
+        }
+    }
+
+    @Override
+    public void sendTaskerEmail(EmailRequest emailRequest) {
+        EmailRequest.EmailType emailType=emailRequest.getEmailType();
+        switch (emailType){
+            case TASK_REQUEST -> sendNotification(emailTemplate.buildEmailSubject(emailRequest), emailTemplate.buildTaskRequestBody(emailRequest.getTask()), emailRequest);
+            case TASK_RESCHEDULE -> sendNotification(emailTemplate.buildEmailSubject(emailRequest), emailTemplate.buildTaskerTaskRescheduleBody(emailRequest.getTask()), emailRequest);
+            default -> {
+                log.error("Unsupported email type: {}", emailType);
+                return;
+            }
+
+        }
+    }
+
+
 }
