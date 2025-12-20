5498c8619fdf553b26c862cca760aea51d04cca4
SamaaIbrahim <173613399+SamaaIbrahim@users.noreply.github.com>
2025-12-20 16:37:01 +0200

    add local changes

---
+++ homemate-backend/src/main/java/com/homemate/notification/domains/model/EmailType.java
@@ -0,0 +1,10 @@
+package com.homemate.notification.domains.model;
+
+public enum EmailType {
+    TASK_RESCHEDULE,
+    TASK_STATUS,
+    TASK_REQUEST,
+    EMAIL_VERIFICATION,
+    FORGOT_PASSWORD,
+    TASK_RESUMED
+}
---
+++ homemate-backend/src/main/java/com/homemate/notification/domains/model/RecipientType.java
@@ -0,0 +1,6 @@
+package com.homemate.notification.domains.model;
+
+public enum RecipientType {
+    USER,
+    TASKER
+}
