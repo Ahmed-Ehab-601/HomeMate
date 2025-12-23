efdf30ec75f77cf57552f3d1497527be392d9ce2
mahmoud842 <mahmoud1hesham2003@gmail.com>
2025-12-23 16:37:19 +0200

    remove unused imports

--- homemate-backend/src/test/java/com/homemate/analysis/controller/UserAnalysisControllerTest.java
+++ homemate-backend/src/test/java/com/homemate/analysis/controller/UserAnalysisControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
